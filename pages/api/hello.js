e6e15c9d5cf22be2da96e327ee8ea936de285b49
Brandon Wong <brandonwongck@gmail.com>
2022-04-12 15:25:24 +0800

    Set cache headers

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  res.setHeader('Cache-Control', 's-maxage=86400');
   res.status(200).json({ name: 'John Doe' })
 }
